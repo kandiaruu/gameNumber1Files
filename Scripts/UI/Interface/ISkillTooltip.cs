8e28eefb88058ecf30156fbf151ca28fb517aed1
kandiaruu <shterlyayev.school.com>
2025-03-23 16:53:32 +0100

    улучшение гибкости (Dependency Injection)

--- Scripts/UI/Interface/ISkillTooltip.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public interface ISkillTooltip
-{
-    void ShowTooltip(Skill skill, Vector3 mousePosition);
-    void UpdatePosition(Vector3 mousePosition);
-    bool IsOpen { get; }
-}
--- Scripts/UI/Interface/ISkillTreeManager.cs
+++
@@ -1,9 +0,0 @@
-public interface ISkillTreeManager
-{
-    void UnlockSkill(int skillIndex);
-    void BuyQuestionState(int skillIndex);
-    int GetSkillPoints();
-    Skill[] GetAllSkills();
-    void EnableSkillButtons(bool enable);
-    void OnNotificationPanelClosed();
-}
